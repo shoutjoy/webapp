c948e033994b2b3dce2c20352f9b2593df2072e2
jhpark <76821476+shoutjoy@users.noreply.github.com>
2022-07-09 04:56:49 +0900

    이름입력기

--- abcontest/app.py
+++ abcontest/app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField,  validators
+
 app = Flask(__name__)
 
+
+class HelloForm(Form):
+    sayhello = TextAreaField('',[validators.DataRequired()])
+
+
 @app.route('/')
+def index():
+    form = HelloForm(request.form)
+    return render_template('first_app.html', form = form)
 
-def hello_pybo():
-    return render_template('index.html')
+@app.route('/hello', methods =['POST'])
+def hello():
+    form = HelloForm(request.form)
+    if request.method =='POST' and form.validate():
+        name = request.form['sayhello']
+        return render_template('hello.html', name = name)
+    return render_template('first_app.html', form = form)
+    
 
-if __name__ =='__main__':
-    app.run()
+# def hello_pybo():
+#     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
